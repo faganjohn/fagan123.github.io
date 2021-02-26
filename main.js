9b9524f3d56e4c8e19f02b5e31e4b0139d96e33a
faganjohn <75076607+faganjohn@users.noreply.github.com>
2021-02-26 06:06:26 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,60 @@
-menu_list_array - ["Chicken Tandoori Pizza", 
-"Veg Supreme Pizza",
- "Paneer Tikka Pizza", 
- "Deluxe Veggie Pizza", 
-"Veg Extravaganza Pizza"];
 
-function getmenu()
-{ var htmldata;
-     htmldata="<ol class='menulist'>"
-      menu_list_array.sort(); 
-      for(var i=0;i<menu_list_array.length;i++)
-      { htmldata+htmldata+'<li>' + menu_list_array[i] + '</li>'
-     } htmldata=htmldata+"</ol>" 
-     document.getElementById("display_menu").innerhtml = htmldata; 
-}
+var last_position_of_x, last_position_of_y;
+
+    canvas = document.getElementById('myCanvas');
+    ctx = canvas.getContext("2d");
+    
+    color = "black";
+    width_of_line = 2;
+
+    var width = screen.width;
+    new_width= screen.width-70;
+    new_height=screen.height-300;
+    if(width<992){
+        document.getElementById("myCanvas").width=new_width;
+        document.getElementById("myCanvas").height=new_height;
+        document.body.style.overflow="hidden";
+    }
+
+    canvas.addEventListener("touchstart", my_touchstart);
+    
+    function my_touchstart(e)
+    {
+        console.log("my_touchstart");
+
+        //Addictonal Activity start
+        color = document.getElementById("color").value;
+        width_of_line = document.getElementById("width_of_line").value;
+        //Addictonal Activity ends
+
+       last_position_of_x=e.touches[0].clientX-canvas.offsetLeft;
+       last_position_of_y=e.touches[0].clientY-canvas.offsetTop;
+    }
+
+
+    canvas.addEventListener("touchmove", my_touchmove);
+    function my_touchmove(e)
+    {
+        console.log("mytouchMove");
+        
+        current_position_of_touch_x=e.touches[0].clientX-canvas.offsetLeft;
+        current_position_of_touch_y=e.touches[0].clientY-canvas.offsetTop;
+
+        ctx.beginPath();
+        ctx.strokeStyle=color;
+        ctx.lineWidth=width_of_line;
+        console.log("Last position of x and y coordinates = ");
+         console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
+          ctx.moveTo(last_position_of_x, last_position_of_y);
+           console.log("Current position of x and y coordinates = ");
+            console.log("x = " + current_position_of_touch_x + "y = " + current_position_of_touch_y);
+             ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
+              ctx.stroke();
+
+              
+              last_position_of_x=current_position_of_touch_x;
+              last_position_of_y=current_position_of_touch_y;
+    }
+
+
+    
